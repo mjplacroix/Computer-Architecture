8100eca9531eb5dae4e7ef7a56f6d934fb64bc7d
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-07-22 20:00:10 -0400

    passing Thursday tests

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,12 +1,6 @@
 """CPU functionality."""
 import sys
 
-# HALT = 0b01
-# LDI = 0b10000010
-# PRN = 0b01000111
-# NOP = 0b00000000
-# MUL = 0b10100010
-
 class CPU:
     """Main CPU class."""
 
@@ -19,17 +13,7 @@ class CPU:
         self.pc = 0
         self.SP = 7
         self.reg[self.SP] = 0xF4 
-
-
-        # ## branch table 
-        # self.branchtable = {}
-        # ## w/ each of the functions inserted
-        # self.branchtable[HALT] = self.halt
-        # self.branchtable[LDI] = self.ldi
-        # self.branchtable[PRN] = self.prn
-        # self.branchtable[NOP] = self.nop
-        # self.branchtable[MUL] = self.mul
-
+        self.FL = [0] * 8
 
     def load(self): 
         # """Load a program into memory."""
@@ -89,11 +73,35 @@ class CPU:
         elif op == "MUL":
             # print(reg_a, reg_b)
             # print(self.reg[reg_a], self.reg[reg_b])
-            print(self.reg)
             self.reg[reg_a] *= self.reg[reg_b]
-            print(self.reg)
         elif op == 'CMP':
-            pass
+            # print(f'CMP: {self.reg[reg_a], self.reg[reg_b]}')
+            if self.reg[reg_a] == self.reg[reg_b]:
+                # print('this_1')
+                # set the 7th index [E] to 1
+                self.FL[7] = 1
+                # # no shift - increment E by 1
+                # self.FL += 1
+                # print(f'CMP[7] -> {self.FL}')
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                # print('this_2')
+                # set the 5th index to 1
+                self.FL[5] = 1
+                # self.FL = self.FL >> 2
+                # # increment L in FLAG by 1
+                # self.FL = 1
+                # # shift FLAG left by 2
+                # self.FL = self.FL << 2
+            else:
+                # print('this_3')
+                # set the 6th index [G] to 1
+                self.FL[6] = 1
+                # # shift FLAG right by 1
+                # self.FL = self.FL >> 1
+                # # increment G in FLAG by 1
+                # self.FL = 1
+                # # shift FLAG left by 1
+                # self.FL = self.FL << 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -116,31 +124,6 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-    
-    ## new functions written for all 5 of these statements so far
-
-    def halt(self):
-        sys.exit()
-
-    def ldi(self, pc):
-        operand_a, operand_b = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
-        self.reg[operand_a] = operand_b
-        self.pc += 3
-    
-    def prn(self, pc):
-        print(self.reg[self.pc])
-        self.pc += 1
-
-    def nop(self, pc):
-        self.pc += 1
-        pass
-
-    def mul(self, pc):
-        self.pc += 1
-        self.alu('MUL', self.ram[self.pc] , self.ram[self.pc + 1])
-        self.pc += 1
-
-
 
     def run(self):
         """Run the CPU."""
@@ -155,21 +138,9 @@ class CPU:
         CMP = 0b10100111
         PUSH = 0b01000101
         POP = 0b01000110
-
-        # Using ram_read(), read the bytes at PC+1 and PC+2 from RAM into variables operand_a and operand_b in case the instruction needs them.
-        # operand_a, operand_b = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
-
-
-        # # self.branchtable[HALT]()
-        # print('2')
-        # self.branchtable[LDI](self.pc)
-        # print('3')
-        # self.branchtable[PRN](self.pc)
-        # print('4')
-        # self.branchtable[NOP](self.pc)
-        # print('5')
-        # self.branchtable[MUL](self.pc)
-        # print('6')
+        JMP = 0b01010100
+        JEQ = 0b01010101
+        JNE = 0b01010110
 
         running = True
 
@@ -180,6 +151,7 @@ class CPU:
             elif ir == LDI:
                 operand_a, operand_b = self.ram_read(self.pc + 1), self.ram_read(self.pc + 2)
                 self.reg[operand_a] = operand_b
+                # print(self.reg, operand_b)
                 self.pc += 3
             elif ir == PRN:
                 self.pc += 1
@@ -195,6 +167,7 @@ class CPU:
             elif ir == CMP:
                 self.pc += 1
                 self.alu('CMP', self.ram[self.pc], self.ram[self.pc +1])
+                self.pc += 2
             elif ir == PUSH:
                 self.pc += 1
                 self.reg[self.SP] -= 1
@@ -214,6 +187,45 @@ class CPU:
                 # Increment SP
                 self.reg[self.SP] += 1
                 self.pc += 1
+            elif ir == JMP:
+                
+                # Jump to the address stored in the given register.
+                new_address = self.reg[self.ram[self.pc + 1]]
+
+                # Set the PC to the address stored in the given register.
+                self.pc = new_address
+            elif ir == JEQ:
+                # if E on FLAG is true
+                # # make a copy for manipulation then comparison
+                # copyFL = self.FL
+                # copyFL = copyFL << 7
+                # copyFL = copyFL >> 7
+                # if the 7th index is equal to 1
+                if self.FL[7] == 1:
+                    # go to registar entry for next pc
+                    self.pc = self.reg[self.ram[self.pc + 1]]
+                else:
+                    self.pc += 2
+            elif ir == JNE:
+                # if E on FLAG is false
+                # # make a copy for manipulation then comparison
+                # copyFL_2 = self.FL
+                # print(f'flag: {copyFL_2}')    # 0b00000000
+                # copyFL_2 = copyFL_2 << 19
+                # print(f'flag: {copyFL_2}')
+                # copyFL_2 = copyFL_2 >> 8
+                # print(f'flag: {copyFL_2}')
+                # if copyFL_2 == 0:
+                # print(f'FL-> {self.FL}')
+                if self.FL[7] == 0:
+                    # print(f'pc -> {self.pc}')
+                    # go to registar entry for next pc
+                    self.pc = self.reg[self.ram[self.pc + 1]]
+                    # print(f'pc -> {self.pc}    ram -> {self.ram[self.pc]}')
+                else:
+                    self.pc += 2
+
+
 
 
                 
